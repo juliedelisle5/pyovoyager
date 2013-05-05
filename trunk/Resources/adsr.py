fff3ec75ca6a5311c276e42e72bb9f14491cb93f
delisle.julie <delisle.julie@b7c4a26f-3832-c29c-a4d6-cdb794cc1581>
2013-05-05 15:00:37 +0000

    Répertoire des méthodes et des instructions pour l'interface graphique dans le fichier Interface-Graphique.txt. Réunification des deux classes enveloppe en une seule; ajout de méthodes pour plus de possibilités de contrôle. Petites corrections à mixer.py et audio.py.

--- trunk/Resources/adsr.py
+++ trunk/Resources/adsr.py
@@ -2,77 +2,90 @@ from pyo import *
 from mixer import *
 
 #Chaines audio du synthetiseur analogique, avec et sans controleur MIDI:
-#   generateurs de son/bruit --> mixeur --> filtre --> enveloppe ADSR
+#   generateurs de son/bruit --> mixeur --> filtre --> enveloppe ADSR de volume
 #Enveloppe est donc la classe "finale", dont le output.
 
 #Appels de methodes sur les generateurs de son/bruit:
-#   objetEnveloppe.mixer.methode(valeur)
+#   enveloppe.mixer.methode(valeur)
 
 #Appels de methodes sur le filtre:
-#   objetEnveloppe.filtre.methode(valeur)
+#   enveloppe.filtre.methode(valeur)
 
 
 #Mode 1 (Pour usage avec un controleur MIDI)
-class KeyboardEnv():
+class VolumeEnv():
     def __init__(self, mul=0.8):
         self.mul = Sig(value=mul)
         self.midi = Notein(scale=1)
-        self.mixer = MixerSection(ref_freq = self.midi['pitch']).stop()
-        self.midi_adsr = MidiAdsr(self.midi['velocity'],mul=self.mul)
-        self.filtre = Filter(self.mixer.getOut(),amp=self.midi_adsr).stop()
-        
-    #Methodes directes de l'objet MidiAdsr:
-    #   self.midi_adsr.setAttack(x) pour la duree d'attaque
-    #   self.midi_adsr.setDecay(x) pour la duree du decay
-    #   self.midi_adsr.setRelease(x) pour la duree du release
-    def setMasterVolume(self,x):
-        self.mul.value = x
-        
-    def out(self):
-        self.mixer.play() 
-        self.filtre.out()
-        return self
-        
-    def stop(self):
-        self.mixer.stop()
-        self.filtre.stop()
-        
-        
-#Mode 2 (Dans le cas ou l'utilisateur n'a pas ou ne veut pas de controleur MIDI):
-#   Ne pas oublier d'appeler la methode play() sur l'objet cree pour partir la note, et stop() pour l'arreter!
-class AutoEnv():
-    def __init__(self, mul=0.8):
-        self.mul = Sig(value=mul)
+        self.mixer = MixerSection(ref_freq = self.midi['pitch']).stop() #Controle clavier de la frequence desactive par defaut
         self.adsr = Adsr(attack=.01, decay=.2, sustain=.5, release=.1, dur=0, mul=.5)
-        self.mixer = MixerSection(130.,0.).stop()
-        self.filtre = Filter(self.mixer.getOut(),amp=self.adsr).stop()
+        self.midi_adsr = MidiAdsr(self.midi['velocity'],mul=self.mul)
+        self.filtre = Filter(self.mixer.getOut(),amp=self.midi_adsr).stop() #Controle clavier de l'amplitude desactive par defaut
+        self.mix = Mix(input=[self.mixer.osc3.getOut()], voices=2, mul=self.mul).stop()
         
-    #Methodes:
-    #   adsr.play() pour partir la note
-    #   adsr.stop() pour partir le release
-    #   adsr.setAttack(x) pour la duree d'attaque
-    #   adsr.setDecay(x) pour la duree du decay
-    #   adsr.setRelease(x) pour la duree du release
+    #Volume master
     def setMasterVolume(self,x):
         self.mul.value = x
-
+        
     def out(self):
         self.mixer.play() 
         self.filtre.out()
         return self
-
+        
     def stop(self):
         self.mixer.stop()
         self.filtre.stop()
+        
+    #Play et stop note quand le controle clavier est desactive
+    def playNote(self):
+        self.adsr.play()
+        
+    def stopNote(self):
+        self.adsr.stop()
+        
+    #Activer ou desactiver le controle de l'enveloppe d'amplitude par les touches du controleur MIDI
+    #Actif par defaut
+    def kbAmpCtlOn(self):
+        self.filtre.amp = self.midi_adsr
+            
+    def kbAmpCtlOff(self):
+        self.filtre.amp = self.adsr
+        
+    #Activer ou desactiver le controle de la frequence de la note jouee par les touches du controleur MIDI
+    #Actif par defaut
+    def kbFreqCtlOn(self):
+        self.mixer.ref_freq = self.midi['pitch']
+        
+    def kbFreqCtlOff(self):
+        self.mixer.ref_freq = 442.
+        
+    #Permet d'activer ou de desactiver le controle clavier du 3e oscillateur, pour en faire un drone ou une pedale.
+    def kb3ControlOn(self):
+        self.mix.stop()
+        self.mixer.osc3.ref_freq = self.midi['pitch']
+
+    def kb3ControOff(self):
+        self.mixer.osc3.ref_freq = 111.
+        self.mix.out()
+    
+    #Parametres de l'enveloppe ADSR de volume
+    def setAttack(self,x):
+        self.midi_adsr.setAttack(x)
+        self.adsr.setAttack(x)
+        
+    def setDecay(self,x):
+        self.midi_adsr.setDecay(x)
+        self.adsr.setDecay(x)
+        
+    def setSustain(self,x):
+        self.adsr.setSustain(x)
+        
+    def setRelease(self,x):
+        self.midi_adsr.setRelease(x)
+        self.adsr.setRelease(x)
 
 
 if __name__ == '__main__':
     s = Server(duplex=1).boot() 
-    output1 = KeyboardEnv()
-    output2 = AutoEnv()
-    mode = 1
-    if mode == 1:
-        output1.out()
-    elif mode == 2:
-        output2.out()
+    enveloppe = VolumeEnv().out()
     s.gui(locals())
